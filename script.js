b3ec2d36fb94fad6551a061966e69d88e24d3077
JOS <sprukts.janis7@gmail.com>
2023-05-17 20:34:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// const cells = document.querySelectorAll('.cell'); atrod masivu html faila
+
+// for (const cell of cells) {   izsauc masivus
+//   cell.addEventListener('click', clickHandle);   pieskir kliksi lai parvedotu masivu
+// }
+const popup = document.querySelector('.popup');
+const message_el = popup.querySelector('.message');
+const board = document.querySelector('.game_board');
+const cells = board.children;
+board.addEventListener('click', boardClickHandle);
+
+function boardClickHandle(event) {
+  if (!event.target.classList.contains('cell')) {
+    return;
+  }
+
+  clickHandle.bind(event.target)();
+}
+
+let symbol = 'x';
+let referee = new Referee();
+let storage = new Storage('tictactoe');
+let moves = {};
+
+function clickHandle() {
+  if (this.textContent !== '') {
+    return;
+  }
+  this.textContent = symbol;
+  const id = this.dataset.id;
+  moves[id] = symbol;
+  storage.create(
+    {symbol: symbol}
+  );
+  if (referee.checkWinner(moves, symbol)) {
+    showMessage("Player " + symbol + ' has won the game!');
+  }
+
+  symbol = (symbol === 'x') ? 'o' : 'x';
+
+  // console.log(moves);
+}
+
+document.querySelector('.reset').addEventListener('click', resetHandle);
+document.querySelector('.reset_game').addEventListener('click', resetHandle);
+
+popup.addEventListener('click', function (event) {
+  if (event.target.classList.contains('popup')) {
+    hideMessage();
+  }
+})
+
+function resetHandle () {
+  for (const cell of cells) {
+    cell.textContent = '';
+  }
+  symbol = 'x';
+  moves = {};
+  hideMessage();
+}
+
+function showMessage(message) {
+  popup.classList.add('open');
+  message_el.textContent = message;
+}
+
+function hideMessage() {
+  popup.classList.remove('open');
+}
+
+
+
+
+
+
